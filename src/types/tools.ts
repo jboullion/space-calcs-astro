867fb2e992c6a6a7f5706ef8446e3addd535b72b
jboullion <jboullion83@gmail.com>
2023-03-21 09:20:34 -0500

    Testing astro layout, imports, types, pages

---
+++ src/types/tools.ts
@@ -0,0 +1,14 @@
+export type SpaceTool = {
+  name: string;
+  description: string;
+  category: string;
+  image: string;
+  link: string;
+};
+
+// export type HeroProps = {
+//   title: string;
+//   description: string;
+//   image: string;
+//   alt: string;
+// };
