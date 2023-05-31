978e869344999575cb35725bd9830d13f9309e0a
jboullion <jboullion83@gmail.com>
2023-05-31 17:03:07 -0500

    refactor Space elevator to better layout

---
+++ src/components/vue/space-elevator/SEForm.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="se-form" class="py-2">
+    <NumberInput
+      id="planet-radius"
+      label="Planet Radius"
+      v-model.number="formData.planetRadius"
+      :step="1"
+      :min="100"
+      :max="1000000"
+      unit="km"
+    />
+
+    <NumberInput
+      id="planet-density"
+      label="Planet Desnity"
+      v-model.number="formData.planetDensity"
+      :step="1"
+      :min="1"
+      :max="100"
+      unit="kg"
+    />
+
+    <NumberInput
+      id="planet-rotation"
+      label="Planet Rotation"
+      v-model.number="formData.planetRotation"
+      :step="1"
+      :min="1"
+      :max="1000"
+      unit="days"
+    />
+
+    <NumberInput
+      id="car-speed"
+      label="Car Speed"
+      v-model.number="formData.carSpeed"
+      :step="1"
+      :min="1"
+      :max="1000"
+      unit="km/h"
+    />
+  </div>
+</template>
+<script setup lang="ts">
+import { defineProps } from "vue";
+import type { SpaceElevatorForm } from "./types";
+import NumberInput from "../forms/NumberInput.vue";
+
+defineProps<{
+  formData: SpaceElevatorForm;
+}>();
+</script>
---
+++ src/components/vue/space-elevator/SEVisual.vue
@@ -0,0 +1,306 @@
+<template>
+  <div
+    id="space-elevator-canvas"
+    class="canvas-wrapper border"
+    style="position: relative; height: 500px"
+  >
+    <i v-if="loading" class="fas fa-cog fa-spin mb-0 h1"></i>
+  </div>
+</template>
+<script setup lang="ts">
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { computed, onBeforeMount, onBeforeUnmount, onMounted, ref } from "vue";
+import type { SpaceElevatorForm } from "./types";
+
+defineProps<{
+  formData: SpaceElevatorForm;
+}>();
+
+const loading = ref(true);
+
+interface Textures {
+  space: THREE.Texture | null;
+  earth: THREE.Texture | null;
+  mars: THREE.Texture | null;
+  moon: THREE.Texture | null;
+}
+
+const textures: Textures = {
+  space: null,
+  earth: null,
+  mars: null,
+  moon: null,
+};
+
+const planet = {
+  gravityConstant: 0.000000000066743, //6.67408 * Math.pow(10, -11), // m3 kg-1 s-2
+  mesh: new THREE.Mesh(),
+  clouds: new THREE.Mesh(),
+  axis: new THREE.Vector3(),
+  geometry: new THREE.SphereGeometry(),
+  material: new THREE.Material(),
+  group: new THREE.Group() as THREE.Group,
+};
+
+const three = {
+  canvas: null as HTMLElement | HTMLCanvasElement | null,
+  renderer: null as THREE.WebGLRenderer | null,
+  scene: new THREE.Scene(),
+  camera: new THREE.PerspectiveCamera(),
+  controls: null as OrbitControls | null,
+  // group: null,
+  // stats: null, // used for debugging
+  // gui: null, // used for debugging
+  // raycaster: null,
+  // mouse: null,
+  minMovement: null as THREE.Vector3 | null,
+  maxMovement: null as THREE.Vector3 | null,
+};
+
+const formData = ref<SpaceElevatorForm>({
+  planetRadius: 6378.14,
+  planetDensity: 5514,
+  planetGravity: 9.807,
+  // planetMass: 5.972 * Math.pow(10, 24),
+  // planetGravity: 9.807,
+  planetRotation: 0.0000729211585532747,
+  carSpeed: 100, //km/h
+  payloadMass: 1000, //kg
+});
+
+const animation = ref({
+  FPS: 60, // In order to ensure things run smoothly on all devices we need to set a fixed framerate
+  prevTick: 0, // track the last tick timestamp
+});
+
+const textureDir = "/textures/";
+
+/**
+ *
+ *
+ *
+ * SETUP
+ *
+ *
+ *
+ */
+
+onBeforeMount(() => {
+  //calcOrbitalVelocity();
+});
+
+onMounted(() => {
+  loadModels();
+  window.addEventListener("resize", setupScene, { passive: true });
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener("resize", setupScene);
+});
+
+/**
+ *
+ *
+ *
+ * COMPUTED
+ *
+ *
+ *
+ */
+
+const elevatorHeight = computed(() => {
+  // TODO: Calculate the planet geostationary height
+  return 36000;
+});
+
+const scaledPlanetRadius = computed(() => {
+  // TODO: We may want to manipulate the radius of the planet to make it easier to work with in Three.js
+  return formData.value.planetRadius;
+});
+
+/**
+ *
+ *
+ *
+ * METHODS
+ *
+ *
+ *
+ */
+
+async function loadModels() {
+  const textureLoader = new THREE.TextureLoader();
+  // //this.textures.space = await textureLoader.load('/wp-content/themes/nexus-aurora/assets/images/2k_stars.jpg');
+  textures.earth = await textureLoader.load(textureDir + "2k_earth_daymap.jpg");
+  textures.mars = await textureLoader.load(textureDir + "2k_mars.jpg");
+  textures.moon = await textureLoader.load(textureDir + "2k_moon.jpg");
+
+  loading.value = false;
+  setupScene();
+}
+
+// function setupAxesHelper() {
+//   if (!three.scene) return;
+//   const axesHelper = new THREE.AxesHelper(scaledPlanetRadius.value / 10); // 500 is size
+//   axesHelper.position.set(
+//     scaledPlanetRadius.value + scaledPlanetRadius.value / 10,
+//     0,
+//     0
+//   );
+//   //this.three.group.add(axesHelper);
+//   three.scene.add(axesHelper);
+// }
+
+function removeAllChildNodes(parent: HTMLElement) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+function setupScene() {
+  if (loading.value) return;
+
+  if (three.scene && three.canvas) {
+    removeAllChildNodes(three.canvas);
+  }
+
+  setupThreeJS();
+
+  setupPlanet();
+
+  if (!animation.value.prevTick) {
+    animate();
+  }
+}
+
+function setupThreeJS() {
+  three.scene = new THREE.Scene();
+  planet.group = new THREE.Group();
+
+  // Renderer
+  three.renderer = new THREE.WebGLRenderer({
+    antialias: true,
+    logarithmicDepthBuffer: true,
+  }); // { alpha: true }
+  three.canvas = document.getElementById("space-elevator-canvas");
+
+  if (!three.canvas) return;
+
+  three.canvas.appendChild(three.renderer.domElement);
+
+  const width = three.canvas.getBoundingClientRect().width;
+  const height = 400;
+
+  three.renderer.setSize(width, height);
+
+  updateCamera();
+
+  // Lights
+  three.scene.add(new THREE.AmbientLight(0x404040));
+  const light = new THREE.DirectionalLight(0xffffff, 0.5);
+  light.position.set(0, 0, 1);
+  three.scene.add(light);
+
+  // // GUI
+  // this.three.gui = new dat.GUI( { autoPlace: false } );
+  // this.three.canvas.appendChild(this.three.gui.domElement);
+}
+
+function updateCamera() {
+  if (!three.renderer) return;
+
+  // Camera
+  const cameraPositionDistance = elevatorHeight.value;
+  const cameraZoomDistance = elevatorHeight.value * 2;
+  let rendererSize = new THREE.Vector2();
+  three.renderer.getSize(rendererSize);
+  three.camera = new THREE.PerspectiveCamera(
+    45,
+    rendererSize.width / rendererSize.height,
+    0.1,
+    cameraZoomDistance * 2
+  );
+
+  three.minMovement = new THREE.Vector3(
+    -cameraZoomDistance,
+    -cameraZoomDistance,
+    -cameraZoomDistance
+  );
+  three.maxMovement = new THREE.Vector3(
+    cameraZoomDistance,
+    cameraZoomDistance,
+    cameraZoomDistance
+  );
+
+  three.camera.position.z = cameraPositionDistance;
+  // this.three.controls.enableZoom = false;
+
+  // Controls
+  three.controls = new OrbitControls(three.camera, three.renderer.domElement);
+}
+
+function setupPlanet() {
+  planet.group = new THREE.Group();
+  planet.axis = new THREE.Vector3(0, 1, 0); // TODO: formData.location.axis
+
+  planet.material = new THREE.MeshLambertMaterial({
+    map: textures.earth,
+  });
+
+  // switch (formData.value.location.name) {
+  //   case "Earth":
+  //     planet.material = new THREE.MeshLambertMaterial({
+  //       map: textures.earth,
+  //     });
+  //     break;
+  //   case "Mars":
+  //     planet.material = new THREE.MeshLambertMaterial({
+  //       map: textures.mars,
+  //     });
+  //     break;
+  //   case "Moon":
+  //     planet.material = new THREE.MeshLambertMaterial({
+  //       map: textures.moon,
+  //     });
+  //     break;
+  // }
+
+  planet.geometry = new THREE.SphereGeometry(scaledPlanetRadius.value, 64, 64);
+
+  //planet.material.transparent = true;
+
+  planet.mesh = new THREE.Mesh(
+    planet.geometry,
+    planet.material as THREE.Material
+  );
+
+  planet.group.add(planet.mesh);
+  three.scene.add(planet.group);
+}
+
+function animate() {
+  if (!three.renderer) return;
+  if (!three.controls) return;
+
+  requestAnimationFrame(animate);
+
+  three.controls.update();
+
+  //this.three.camera.position.clamp(this.three.minMovement, this.three.maxMovement);
+  three.renderer.render(three.scene, three.camera);
+
+  //if (formData.value.pause) return;
+
+  // clamp to fixed framerate
+  const now = Math.round(
+    (animation.value.FPS * window.performance.now()) / 1000
+  );
+
+  if (now == animation.value.prevTick) return;
+
+  animation.value.prevTick = now;
+
+  // planet.group.rotateOnAxis(planet.axis, rotationSpeed.value);
+}
+</script>
