bbb317829375d3898a069ddc2d7b4e99b6fdd4e6
jboullion <jboullion83@gmail.com>
2023-08-22 22:22:27 -0500

    Update logo

--- src/components/vue/home/Home.vue
+++ src/components/vue/home/Home.vue
@@ -1,74 +1,79 @@
 <template>
-  <div>
-    <section class="py-5 text-center bg-black text-light" id="home-canvas">
-      <div id="homeAccents">
-        <img src="/images/sun.jpg" id="homeSun" alt="" />
-        <img src="/images/earth.jpg" id="homeEarth" alt="" />
-      </div>
-      <div class="container position-relative">
-        <div class="row">
-          <div class="col-lg-6 col-md-8 mx-auto">
-            <div class="d-flex justify-content-center gap-3">
-              <!-- <img src="/logo.svg" alt="Space Calcs" height="70" width="30" /> -->
-              <Logo />
-              <h1 class="fw-light">Space Calcs</h1>
+    <div>
+        <section class="py-5 text-center bg-black text-light" id="home-canvas">
+            <div id="homeAccents">
+                <img src="/images/sun.jpg" id="homeSun" alt="" />
+                <img src="/images/earth.jpg" id="homeEarth" alt="" />
             </div>
+            <div class="container position-relative">
+                <div class="row">
+                    <div class="col-lg-6 col-md-8 mx-auto">
+                        <div class="d-flex justify-content-center gap-3">
+                            <img
+                                id="homeLogo"
+                                src="/logo.svg"
+                                alt="Space Calcs"
+                            />
+                            <!-- <Logo /> -->
+                            <!-- <h1 class="fw-light">Space Calcs</h1> -->
+                        </div>
 
-            <p class="lead my-4">
-              All the tools you need to calculate your mission to space
-            </p>
+                        <p class="lead my-4">
+                            All the tools you need to calculate your mission to
+                            space
+                        </p>
 
-            <div class="row">
-              <div class="col-md-6 mb-3">
-                <div class="input-group">
-                  <input
-                    type="text"
-                    class="form-control"
-                    placeholder="Search for a tool"
-                    aria-label="Search for a tool"
-                    v-model="search"
-                  />
-                  <span class="input-group-text"
-                    ><i class="fas fa-search"></i
-                  ></span>
+                        <div class="row">
+                            <div class="col-md-6 mb-3">
+                                <div class="input-group">
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        placeholder="Search for a tool"
+                                        aria-label="Search for a tool"
+                                        v-model="search"
+                                    />
+                                    <span class="input-group-text"
+                                        ><i class="fas fa-search"></i
+                                    ></span>
+                                </div>
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <select
+                                    class="form-select"
+                                    placeholder="Select Category"
+                                    aria-label="Select Category"
+                                    v-model="selectedCategory"
+                                    @change="toggleCategory()"
+                                >
+                                    <option value="" selected>All</option>
+                                    <option
+                                        v-for="category in categories"
+                                        :key="category.slug"
+                                        :value="category.slug"
+                                    >
+                                        {{ category.name }}
+                                    </option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-              <div class="col-md-6 mb-3">
-                <select
-                  class="form-select"
-                  placeholder="Select Category"
-                  aria-label="Select Category"
-                  v-model="selectedCategory"
-                  @change="toggleCategory()"
-                >
-                  <option value="" selected>All</option>
-                  <option
-                    v-for="category in categories"
-                    :key="category.slug"
-                    :value="category.slug"
-                  >
-                    {{ category.name }}
-                  </option>
-                </select>
-              </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
+        </section>
 
-    <CalcList :search="search" :active-categories="activeCategories" />
-  </div>
+        <CalcList :search="search" :active-categories="activeCategories" />
+    </div>
 </template>
 
 <script setup lang="ts">
-import * as THREE from "three";
+import * as THREE from 'three';
 
-import CalcList from "./CalcList.vue";
-import Logo from "../shared/Logo.vue";
+import CalcList from './CalcList.vue';
+import Logo from '../shared/Logo.vue';
 
-import { onMounted, ref } from "vue";
-import { categories } from "./constants";
+import { onMounted, ref } from 'vue';
+import { categories } from './constants';
 // import { debounce } from "../../../utils/utils";
 
 // const loading = ref(true);
@@ -99,17 +104,17 @@ import { categories } from "./constants";
 // let threeScene: THREE.Scene | null = null;
 // let threeCamera: THREE.OrthographicCamera | null = null;
 
-const search = ref("");
+const search = ref('');
 
 const activeCategories = ref<string[]>([]);
-const selectedCategory = ref<string>("");
+const selectedCategory = ref<string>('');
 
 function toggleCategory() {
-  if (selectedCategory.value === "") {
-    activeCategories.value = [];
-  } else {
-    activeCategories.value = [selectedCategory.value];
-  }
+    if (selectedCategory.value === '') {
+        activeCategories.value = [];
+    } else {
+        activeCategories.value = [selectedCategory.value];
+    }
 }
 
 // onMounted(() => {
@@ -295,16 +300,16 @@ function toggleCategory() {
 
 <style>
 #home-canvas {
-  position: relative;
+    position: relative;
 }
 
 #home-canvas #homeAccents {
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  position: absolute;
-  top: 0;
-  height: 100%;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    height: 100%;
 }
 
 /* #home-canvas .container {
@@ -323,9 +328,9 @@ function toggleCategory() {
 } */
 
 @media (max-width: 768px) {
-  #home-canvas canvas,
-  #home-canvas #homeAccents {
-    display: none !important;
-  }
+    #home-canvas canvas,
+    #home-canvas #homeAccents {
+        display: none !important;
+    }
 }
 </style>
