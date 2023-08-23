42a9cb0fb92304ecdac68404ba30515e6e5f5885
jboullion <jboullion83@gmail.com>
2023-08-22 22:28:44 -0500

    prevent layout update when loading darkmode

--- src/components/vue/shared/DarkMode.vue
+++ src/components/vue/shared/DarkMode.vue
@@ -1,35 +1,36 @@
 <template>
-  <button
-    type="button"
-    @click="changeTheme()"
-    class="btn btn-dark fs-4"
-    aria-label="Darkmode"
-  >
-    <i v-if="theme === 'light'" class="fa-solid fa-fw fa-moon"></i>
-    <i v-if="theme === 'dark'" class="fa-solid fa-fw fa-sun"></i>
-  </button>
+    <button
+        type="button"
+        @click="changeTheme()"
+        class="btn btn-dark fs-4"
+        aria-label="Darkmode"
+        style="height: 50px"
+    >
+        <i v-if="theme === 'light'" class="fa-solid fa-fw fa-moon"></i>
+        <i v-if="theme === 'dark'" class="fa-solid fa-fw fa-sun"></i>
+    </button>
 </template>
 
 <script setup lang="ts">
-import { onBeforeMount, ref } from "vue";
+import { onBeforeMount, ref } from 'vue';
 
-const theme = ref<"light" | "dark">("light");
+const theme = ref<'light' | 'dark'>();
 
 onBeforeMount(() => {
-  if (localStorage.getItem("theme") == "dark") {
-    theme.value = "dark";
-  }
+    if (localStorage.getItem('theme') == 'dark') {
+        theme.value = 'dark';
+    }
 });
 
 function changeTheme() {
-  if (theme.value == "dark") {
-    theme.value = "light";
-  } else {
-    theme.value = "dark";
-  }
+    if (theme.value == 'dark') {
+        theme.value = 'light';
+    } else {
+        theme.value = 'dark';
+    }
 
-  document.documentElement.setAttribute("data-bs-theme", theme.value);
+    document.documentElement.setAttribute('data-bs-theme', theme.value);
 
-  localStorage.setItem("theme", theme.value);
+    localStorage.setItem('theme', theme.value);
 }
 </script>
