76a5e837edc64fd2018631d07a4e27e31d2922e6
jboullion <jboullion83@gmail.com>
2023-08-25 22:45:29 -0500

    limit simulation speed on orbit visualizer

--- src/components/vue/orbit-visualizer/OrbitVisualizer.vue
+++ src/components/vue/orbit-visualizer/OrbitVisualizer.vue
@@ -68,7 +68,7 @@
                                 v-model.number="formData.simulationSpeed"
                                 :step="1"
                                 :min="1"
-                                :max="100000"
+                                :max="10"
                                 @change="updateSimulationSpeed"
                                 tooltip="How much faster should the simulation run?"
                             />
