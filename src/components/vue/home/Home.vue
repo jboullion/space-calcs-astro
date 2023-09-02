c9466748953f53380d1036ac92464f8d16589f2d
jboullion <jboullion83@gmail.com>
2023-09-02 17:22:50 -0500

    New calculator list design!

--- src/components/vue/home/Home.vue
+++ src/components/vue/home/Home.vue
@@ -1,140 +1,55 @@
 <template>
-    <div>
-        <section class="py-5 text-center bg-black text-light" id="home-canvas">
-            <div id="homeAccents">
-                <img src="/images/sun.jpg" id="homeSun" alt="" />
-                <img src="/images/earth.jpg" id="homeEarth" alt="" />
-            </div>
-            <div class="container position-relative">
-                <div class="row">
-                    <div class="col-lg-6 col-md-8 mx-auto">
-                        <div class="d-flex justify-content-center gap-3">
-                            <img
-                                id="homeLogo"
-                                src="/logo.svg"
-                                alt="Space Calcs"
-                            />
-                            <!-- <Logo /> -->
-                            <!-- <h1 class="fw-light">Space Calcs</h1> -->
-                        </div>
+	<div>
+		<section class="py-5 text-center bg-black text-light" id="home-canvas">
+			<div id="homeAccents">
+				<img src="/images/sun.jpg" id="homeSun" alt="" />
+				<img src="/images/earth.jpg" id="homeEarth" alt="" />
+			</div>
+			<div class="container position-relative">
+				<div class="row">
+					<div class="col-lg-6 col-md-8 mx-auto">
+						<div class="d-flex justify-content-center gap-3">
+							<img
+								id="homeLogo"
+								src="/logo.svg"
+								alt="Space Calcs"
+							/>
+						</div>
 
-                        <p class="lead my-4">
-                            All the tools you need to calculate your mission to
-                            space
-                        </p>
+						<p class="lead my-4">
+							All the tools you need to calculate your mission to
+							space
+						</p>
+					</div>
+				</div>
+			</div>
+		</section>
 
-                        <div class="row">
-                            <div class="col-md-6 mb-3">
-                                <div class="input-group">
-                                    <input
-                                        type="text"
-                                        class="form-control"
-                                        placeholder="Search for a tool"
-                                        aria-label="Search for a tool"
-                                        v-model="search"
-                                    />
-                                    <span class="input-group-text"
-                                        ><i class="fas fa-search"></i
-                                    ></span>
-                                </div>
-                            </div>
-                            <div class="col-md-6 mb-3">
-                                <select
-                                    class="form-select"
-                                    placeholder="Select Category"
-                                    aria-label="Select Category"
-                                    v-model="selectedCategory"
-                                    @change="toggleCategory()"
-                                >
-                                    <option value="" selected>All</option>
-                                    <option
-                                        v-for="category in categories"
-                                        :key="category.slug"
-                                        :value="category.slug"
-                                    >
-                                        {{ category.name }}
-                                    </option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <CalcList :search="search" :active-categories="activeCategories" />
-    </div>
+		<CalcList />
+	</div>
 </template>
 
 <script setup lang="ts">
-import * as THREE from 'three';
-
 import CalcList from './CalcList.vue';
-import Logo from '../shared/Logo.vue';
-
-import { onMounted, ref } from 'vue';
-import { categories } from './constants';
-import { supabase } from '../../../lib/supabaseClient';
-// import {
-//     SUPA_PROJECT_URL,
-//     SUPA_ANON_PUBLIC,
-// } from '../../../utils/public-variables';
-// import { supabase } from '../../../lib/supabaseClient';
-
-const search = ref('');
-
-const activeCategories = ref<string[]>([]);
-const selectedCategory = ref<string>('');
-
-function toggleCategory() {
-    if (selectedCategory.value === '') {
-        activeCategories.value = [];
-    } else {
-        activeCategories.value = [selectedCategory.value];
-    }
-}
-
-onMounted(async () => {
-    // const { data, error } = await supabase.from('calculators').select();
-    // console.log(data, error);
-    // const { data, error } = await supabase.from('countries').select();
-    // console.log(data, error);
-});
 </script>
 
 <style>
 #home-canvas {
-    position: relative;
+	position: relative;
 }
 
 #home-canvas #homeAccents {
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-    position: absolute;
-    top: 0;
-    height: 100%;
+	display: flex;
+	justify-content: space-between;
+	width: 100%;
+	position: absolute;
+	top: 0;
+	height: 100%;
 }
-
-/* #home-canvas .container {
-  position: relative;
-  z-index: 2;
-}
-
-#home-canvas canvas {
-  position: absolute;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 1;
-} */
 
 @media (max-width: 768px) {
-    #home-canvas canvas,
-    #home-canvas #homeAccents {
-        display: none !important;
-    }
+	#home-canvas #homeAccents {
+		display: none !important;
+	}
 }
 </style>
--- src/components/vue/home/constants.ts
+++ src/components/vue/home/constants.ts
@@ -1,7 +1,8 @@
 export interface Category {
-    name: string;
-    slug: string;
-    color: string;
+	name: string;
+	slug: string;
+	color: string;
+	icon: string;
 }
 
 // TODO: We should probably move this to a DB or figure out a better way to do this.
@@ -14,41 +15,48 @@ export const ROCKET_CAT = 5;
 export const STATION_CAT = 6;
 
 export const categories: Category[] = [
-    // TODO: Perhaps split this into a different thing. IE Give each calculator a boolean of "advanced" instead of a category. Possibly hide by deafult until it is toggled on
-    {
-        name: 'Advanced',
-        slug: 'advancd',
-        color: 'dark',
-    },
-    {
-        name: 'Energy',
-        slug: 'energy',
-        color: '',
-    },
-    {
-        name: 'Habitats',
-        slug: 'habitats',
-        color: 'success',
-    },
-    {
-        name: 'Life',
-        slug: 'life',
-        color: 'success',
-    },
+	// TODO: Perhaps split this into a different thing. IE Give each calculator a boolean of "advanced" instead of a category. Possibly hide by deafult until it is toggled on
+	{
+		name: 'Advanced',
+		slug: 'advancd',
+		color: 'danger-emphasis',
+		icon: 'fa-graduation-cap',
+	},
+	{
+		name: 'Energy',
+		slug: 'energy',
+		color: 'warning-emphasis',
+		icon: 'fa-bolt-lightning',
+	},
+	{
+		name: 'Habitats',
+		slug: 'habitats',
+		color: 'secondary-emphasis',
+		icon: 'fa-igloo',
+	},
+	{
+		name: 'Life',
+		slug: 'life',
+		color: 'success',
+		icon: 'fa-seedling',
+	},
 
-    {
-        name: 'Orbits',
-        slug: 'orbits',
-        color: 'info',
-    },
-    {
-        name: 'Rockets',
-        slug: 'rockets',
-        color: 'danger',
-    },
-    {
-        name: 'Stations',
-        slug: 'stations',
-        color: 'primary',
-    },
+	{
+		name: 'Orbits',
+		slug: 'orbits',
+		color: 'primary',
+		icon: 'fa-globe',
+	},
+	{
+		name: 'Rockets',
+		slug: 'rockets',
+		color: 'danger',
+		icon: 'fa-rocket',
+	},
+	{
+		name: 'Stations',
+		slug: 'stations',
+		color: 'white',
+		icon: 'fa-building',
+	},
 ];
