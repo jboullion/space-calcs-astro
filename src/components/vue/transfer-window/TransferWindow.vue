46cc68ad556b5635f44fa009c6965450652576fe
jboullion <jboullion83@gmail.com>
2023-12-11 11:07:05 -0600

    transfew window start

---
+++ src/components/vue/transfer-window/TransferWindow.vue
@@ -0,0 +1,36 @@
+<template>
+	<div
+		id="transfer-window__app"
+		class="row justify-content-center calculator"
+	>
+		<div id="transfer-window__form" class="col-lg-6 m-auto">
+			This is the form
+			<!-- <SaveLoad :data="formData" @loadCalc="loadCalc" /> -->
+		</div>
+		<!-- <div id="transfer-window__results" class="col-lg-8"> -->
+		<!-- <FlyWheelVisual :formData="formData" /> -->
+		<!-- <FlyWheelResults :formData="formData" /> -->
+		<!-- </div> -->
+	</div>
+</template>
+
+<script setup lang="ts">
+// TODO: Must Dos!
+
+// ! BUGS
+
+// ? NOTE: Optional Improvements!
+
+import { onMounted, reactive, ref } from 'vue';
+
+// const formData = ref<IFlyWheelForm>({
+// 	geometry: flyWheelGeometry[0],
+// 	mass: 1000,
+// 	radius: 3,
+// 	rpm: 1000,
+// 	material: materials[0],
+// 	roundTripEfficiency: 0.9,
+// 	powerDraw: 1,
+// });
+</script>
+<style></style>
